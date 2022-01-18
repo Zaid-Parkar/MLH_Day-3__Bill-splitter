21a48eb6645c59524a99a95da893ab31479c9121
Zaid Parkar <zaids.parkar@gmail.com>
2022-01-18 14:57:42 +0530

    Add files via upload

---
+++ bill.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float amount;
+    float people;
+    float share;
+
+
+    cout << "Enter total amount" << endl;
+    cin >> amount;
+
+    cout << "Enter number of peoples" <<endl;
+    cin >> people;
+
+    cout<<"share for each = "<<amount/people<<" Rs"<<endl;
+
+}
